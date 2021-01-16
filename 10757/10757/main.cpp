b124e4f4c204306eba9a39e8e7e4fa431b26e84c
dnjsgud0625 <dnjsgud0625@naver.com>
2021-01-17 00:04:35 +0900

    10757

--- 10757/10757/main.cpp
+++ 10757/10757/main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    string a,b;
-    int ans[10001]={0,};
+    string A,B;
+    int a[10001]={0,},b[10001],rst[10001],carry=0;
     
-    cin >> a >> b;
-    if(a.size()>=b.size()){
-        
+    cin >> A >> B;
+    reverse(A.begin(), A.end());
+    reverse(B.begin(), B.end());
+    
+    for(int i=0;i<A.length();i++){
+        char num = A.at(i);
+        a[i] = atoi(&num);
     }
-    
+    for(int i=0;i<B.length();i++){
+        char num = B.at(i);
+        b[i] = atoi(&num);
+    }
+    for(int i=0;i<max(A.length(),B.length());i++){
+        rst[i] = a[i] + b[i] + carry;
+        if(rst[i] >= 10){
+            rst[i] -= 10;
+            carry = 1;
+        }
+        else carry = 0;
+    }
+    if(carry == 1)  cout << "1";
+    for(int i=max(A.length(),B.length());i>0;i--){
+        cout << rst[i-1];
+    }
+    cout << endl;
     return 0;
 }
