5f60e7ec2fbd1b8bc5bb19db5c600d6b2570b3b0
dnjsgud0625 <dnjsgud0625@naver.com>
2021-01-25 18:44:39 +0900

    2231

--- 2231/2231/main.cpp
+++ 2231/2231/main.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n,num=1,sum;
+    
+    cin >> n;
+    for(int i=1;i<n;i++){
+        num=i;
+        sum=i;
+        while(num){
+            sum += num%10;
+            num /= 10;
+        }
+        if(n==sum){
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << "0" << endl;
     return 0;
 }
