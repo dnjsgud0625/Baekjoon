c4dbb49c539e4adb28436f40a2ce591022a809c7
dnjsgud0625 <dnjsgud0625@naver.com>
2021-02-06 21:11:29 +0900

    11651

---
+++ 11651/11651/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> vec(n);
+    
+    for(int i=0;i<n;i++){
+        cin >> vec[i].second >> vec[i].first;
+    }
+    sort(vec.begin(), vec.end());
+    for(int i=0;i<n;i++)
+    cout << vec[i].second << " " << vec[i].first << '\n';
+    return 0;
+}
